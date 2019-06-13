11cc7515f289e148399a315bfc3c5843582a6321
Ven <venkatsgithub@gmail.com>
2019-06-13 22:32:40 +0530

    Added arrays and variablesAndTypes exercise files

---
+++ Arrays/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+
+    int average, grades[3];
+
+    grades[0]=80;
+    grades[1]=85;
+    grades[2]=90;
+
+    average = (grades[0]+grades[1]+grades[2])/3;
+
+    printf("The average of the 3 grades is %d", average);
+
+    return 0;
+}
