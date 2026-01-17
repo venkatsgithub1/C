a3fe530aff6d2b5008202766fb206a9ac9189443
ven <venkatsgithub@gmail.com>
2026-01-17 22:58:08 +0530

    Corrected quadratic equation roots

--- quadraticEquation/main.c
+++ quadraticEquation/main.c
@@ -5,9 +5,11 @@ int main(void) {
     double a = 1.2;
     double b = 2.3;
     double c = -3.4;
-    double x = 1.4;
 
-    double ans = a*pow(x, 2) + b*x + c;
-    printf("ans = %f", ans);
+    double ans1 = (-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
+    double ans2 = (-b - sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
+
+    printf("ans1 = %f\n", ans1);
+    printf("ans2 = %f\n", ans2);
     return 0;
 }
