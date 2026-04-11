9294801af5a05bd605ba418c005e76f7034abd30
ven <venkatsgithub@gmail.com>
2026-04-11 22:20:32 +0530

    Adding exercise 2 code

---
+++ pointers/exercises/exercise2/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int a = 5;
+    int b = 10;
+
+    printf("Before swap: a = %d, b = %d\n", a, b);
+
+    swap(&a, &b);
+
+    printf("After swap: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
