c498e7f64dbd629ea243640dd7ee96b6f5eb7c68
ven <venkatsgithub@gmail.com>
2026-04-16 23:01:20 +0530

    An example of array of strings

---
+++ strings/arrayofstrings/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void) {
+    char *southIndianStates[] = {"Andhra pradesh", "Karnataka", "Tamilnadu", "Kerala"};
+    int len = sizeof(southIndianStates) / sizeof(char *);
+    for (int i = 0; i < len; i++) {
+        printf("%s\n", southIndianStates[i]);
+    }
+    return 0;
+}
